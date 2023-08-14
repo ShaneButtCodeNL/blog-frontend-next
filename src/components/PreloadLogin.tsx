b014b7020be8074e0bcd934257edb539f94d83b9
shane butt <shanebuttcode@gmail.com>
2023-08-14 17:41:15 -0230

    Updated login client side

--- src/components/PreloadLogin.tsx
+++ src/components/PreloadLogin.tsx
@@ -5,6 +5,7 @@ import { setLoggedIn, setUserDetails } from "@/store/login";
 import { ReactNode, useMemo, useRef, useState } from "react";
 import {
   revalidateTokenFunction,
+  setTokenInCookie,
   validateTokenFunction,
 } from "@/functions/serverFunctions";
 
@@ -19,9 +20,10 @@ export default function PreloadLogin({ children }: { children: ReactNode }) {
         if (localStorage.getItem("userDetails")) {
           validateTokenFunction(token).then((v) => {
             if (v) {
-              revalidateTokenFunction(token).then((t) =>
-                localStorage.setItem("token", t)
-              );
+              revalidateTokenFunction(token).then((t) => {
+                localStorage.setItem("token", t);
+                setTokenInCookie(t);
+              });
               store.dispatch(setLoggedIn(true));
               store.dispatch(
                 setUserDetails(
