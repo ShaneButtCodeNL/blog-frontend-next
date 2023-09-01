4d9ca3ca4eb0ed1ee83f86cfc4c3284b9f5e7bd7
shane butt <shanebuttcode@gmail.com>
2023-09-01 07:49:09 -0230

    styled inputs and login page

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -27,61 +27,66 @@ export default function LoginForm(params: any) {
   }
 
   return (
-    <form onSubmit={(e) => loginSubmit(e)} id="login-form">
-      <label htmlFor="username" id="username-label" className="form-label">
-        Username :{" "}
-      </label>
-      <input
-        type="text"
-        name="username"
-        defaultValue={""}
-        onChange={(e) => setUsername(e.target.value)}
-        id="username-input"
-        className="form-input"
-      />
-      <label htmlFor="password" id="password-label" className="form-label">
-        Password :{" "}
-      </label>
-      <div id="form-input-container">
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          id="password-input"
-          className="form-input"
-          style={!hidden ? { display: "none" } : {}}
-          required
-        />
+    <div id="form-wrapper">
+      <form onSubmit={(e) => loginSubmit(e)} id="login-form">
+        <label htmlFor="username" id="username-label" className="form-label">
+          Username :{" "}
+        </label>
         <input
           type="text"
-          name="password"
-          value={password}
-          id="password-input"
+          name="username"
+          defaultValue={""}
+          onChange={(e) => setUsername(e.target.value)}
+          id="username-input"
           className="form-input"
-          style={hidden ? { display: "none" } : {}}
-          onChange={(e) => setPassword(e.target.value)}
-          required
         />
-        <div id="form-input-container-eye" onClick={() => setHidden((v) => !v)}>
-          {hidden ? (
-            <FontAwesomeIcon icon={faEyeSlash} />
-          ) : (
-            <FontAwesomeIcon icon={faEye} />
-          )}
+        <label htmlFor="password" id="password-label" className="form-label">
+          Password :{" "}
+        </label>
+        <div id="form-input-container">
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password-input"
+            className="form-input"
+            style={!hidden ? { display: "none" } : {}}
+            required
+          />
+          <input
+            type="text"
+            name="password"
+            value={password}
+            id="password-input"
+            className="form-input"
+            style={hidden ? { display: "none" } : {}}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <div
+            id="form-input-container-eye"
+            onClick={() => setHidden((v) => !v)}
+          >
+            {hidden ? (
+              <FontAwesomeIcon icon={faEyeSlash} />
+            ) : (
+              <FontAwesomeIcon icon={faEye} />
+            )}
+          </div>
         </div>
-      </div>
-      <button type="submit" id="login-confirm" className="form-button">
-        Submit
-      </button>
-      <button
-        type="button"
-        id="login-cancel"
-        className="form-button"
-        onClick={() => router.back()}
-      >
-        Cancel
-      </button>
-    </form>
+        <button type="submit" id="login-confirm" className="form-button">
+          Submit
+        </button>
+        <button
+          type="button"
+          id="login-cancel"
+          className="form-button"
+          onClick={() => router.back()}
+        >
+          Cancel
+        </button>
+      </form>
+    </div>
   );
 }
